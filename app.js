aebf9bbcb709ef4d0682d4231fe06e8740f0795c
Naima Ghulam Muhammad <63257312+NaimaGhulamMuhammad@users.noreply.github.com>
2020-06-21 16:20:13 +0500

    assignment done

--- app.js
+++ app.js
@@ -540,3 +540,479 @@
 // alert("Good evening")
 // else if(time >= 2100 && time<= 2300)
 // alert("Good Night")
+//CHAPTER 14-16
+//TASK 1
+// var stdname = {
+//     name1:" ",
+//     name2: " ",
+//     name3: ""
+// }
+
+//TASK 2
+// var arr = [];
+
+//TASK 3
+// var strarr = ["naima","ghulam Muhammad", "21 november"];
+
+//TASK 4
+//  var numarr = [21,67, 87,90,243];
+
+//TASK 5
+// var boolarr = [true, false];
+
+//TASK 9
+// var clr = ["red","blue","pink"];
+// document.write ("<h1>innitial array</h1>")
+// var i;
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// var inp = prompt ("enter a value you want to add at the beginning");
+// clr.unshift(inp);
+// document.write ("<h3>add the value at the beginning of array</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// var inp = prompt ("enter a value you want to add at the end");
+// clr.push(inp);
+// document.write ("<h3>add the value at the beginning of array</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// clr.unshift ("brown","skyblue")
+// document.write ("<h3>add two more colors at the end of array</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// clr.pop(clr[0]);
+// document.write ("<h3>remove the value at the end of array</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// clr.shift(clr[0]);
+// document.write ("<h3>remove the value at the beginning of array</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// var ind = +prompt("enter a index  at which you want to add the color");
+// var inp = prompt ("enter a color name");
+// clr.splice(ind,0,inp);
+// document.write ("<h3>add the color at the desired position</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+// var ind = +prompt("enter a index at which you want to delete");
+// var inp = +prompt ("how many colors you want to delete");
+// clr.splice(ind,inp);
+// document.write ("<h3>remove the color(s) at the desired position</h3>")
+// for(i=0; i<clr.length; i++)
+// document.write(clr[i]+".   ");
+
+//TASK 10
+// var arr = [320,230, 480,120];
+// document.write("SCORES OF STUDENTS: "+arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3]+"<br>");
+// arr.sort();
+// document.write("ORDERED SCORES OF STUDENTS: "+arr[0]+" "+arr[1]+" "+arr[2]+" "+arr[3]+"<br>");
+
+//TASK 11
+// var arr = ["karachi","lahore", "Islamabad","Quetta","peshawar"];
+// var i;
+// var selected = arr.slice(2,4);
+// document.write("<h2>Cities List</h2>")
+// for(i=0; i<arr.length; i++)
+// document.write(arr[i]+".   ");
+// document.write("<h2> Selected Cities List</h2>")
+// for(i=0; i<selected.length; i++)
+// document.write(selected[i]+".   ");
+
+//TASK 12
+// var arr = ["This","is", "My","cat"];
+// var i;
+// document.write("<h2>Array</h2>")
+// for(i=0; i<arr.length; i++)
+// document.write(arr[i]+".   ");
+// var joinfun = arr.join(" ");
+// document.write("<h2> String</h2>")
+// for(i=0; i<joinfun.length; i++)
+// document.write(joinfun[i]);
+
+//TASK 13
+// var devices = ["keyboard", "mouse","printer","monitor"];
+// var i;
+// document.write(<h2>Devices</h2>);
+// for(i = 0; i<devices.length; i++);
+// document.write(devices[i]+" ");
+// for(i = 0; i<devices.length; i++);
+// {
+//     document.write(<h2>OUt</h2>);
+//     var out = devices.shift(arr[i]);
+//     document.write(out);
+// }
+
+//TASK 14
+// var devices = ["keyboard","mouse","printer","monitor"]
+// var i;
+// document.write("<h2>Devices</h2>")
+// for(i = 0; i<devices.length; i++)
+// document.write(devices[i]+",  ")
+// var out;
+// for(i = 0; i<4; i++)
+// {
+//     document.write("<h2>Out: </h2>")
+//     out = devices.pop(devices [i])
+//     document.write(out)
+// }
+
+//TASK 15
+// var arr = ["Samsung","Apple","Motorola","Nokia","Sony& Hier"]
+// var i;
+// document.write("<label >select one brand </label>");
+// document.write ("<select> ")
+// for(i=0; i<arr.length ; i++)
+// document.write ("<option value=arr[0]> "+arr[i]+" </option>")
+// document.write ("</select> ")
+
+                                        //chapter 17-20
+//TASK 1
+// var arr2d = [[],[],[]];
+
+//TASK 2
+// var arr2d =[
+//     [0,1,2,3],
+//     [1,0,1,2],
+//     [2,1,0,1]
+//     ]
+
+//TASK 3
+// var count = 1;
+// for (count = 1;count<=10; count++)
+// document.write(count+" <br>")
+
+//TASK 4
+// var tblnum = prompt ("Enter the number to show its multiplication table")
+// var tbllen = prompt ("Enter length of multiplication table")
+// var i ;
+// document.write ("<h2>Multiplication Table of "+tblnum+" with length "+tbllen+"</h2>")
+// for(i=1; i <= tbllen; i++)
+// document.write(tblnum +" x "+i+" = "+tblnum*i+"<br>");
+
+//TASK 5
+// var fruit = ["apple","banana","mango","orange","strawberry"]
+// var i;
+// for(i=0; i<fruit.length; i++)
+// document.write(fruit[i]+"<br>")
+// document.write ("<br>")
+// for(i=0; i<fruit.length; i++)
+// document.write("Element at index "+i+" is "+fruit[i]+"<br>")
+
+//TASK 6
+// var count = 1;
+// document.write("<h3>counting: \t</h3>")
+// for (count = 1;count<=10; count++)
+// document.write(count+" ,\t")
+
+// document.write("<h3>Reverse counting: \t</h3>")
+// for (count = 10;count>=1; count--)
+// document.write(count+" ,\t")
+
+// document.write("<h3>even Number: \t</h3>")
+// for (count = 0;count<=20; count+=2)
+// document.write(count+" ,\t")
+
+// document.write("<h3>Odd Number: \t</h3>")
+// for (count = 1;count<=20; count+=2)
+// document.write(count+" ,\t")
+
+// document.write("<h3>Series: \t</h3>")
+// for (count = 2;count<=20; count+=2)
+// document.write(count+"k ,\t")
+
+//TASK 7
+// var arr = ["applepie", "cake", "cookie","chips","patties"];
+// var inp = prompt("welcome to my bakery, what do you want to order sir/Ma'm ")
+// for(i=0; i<=arr.length; i++)
+// if(inp === arr[i]){
+// document.write(arr[i]+" is available at "+i+" in our bakery")
+// break;}
+// if(!(inp === arr[i]))
+// document.write("We are Sorry, "+inp+" is not available in our bakery")
+
+//TASK 8
+// var arr = [24,54,8,19,90,40,21];
+// var large = arr[0];
+// var i, j; 
+// document.write("<h2>Array items</h2>")
+// for(i=0; i<arr.length; i++)
+// document.write(arr[i]+" ,")
+// for(i=0; i<arr.length; i++)
+// if(arr[i] > large)
+// large = arr[i];
+// document.write("<h2>largest Number in an array is "+large+"</h2>")
+
+//TASK 9
+// var arr = [24,54,8,19,90,40,21];
+// var small = arr[0];
+// var i, j; 
+// document.write("<h2>Array items</h2>")
+// for(i=0; i<arr.length; i++)
+// document.write(arr[i]+" ,")
+// for(i=0; i<arr.length; i++)
+// if(arr[i] < small)
+// small = arr[i];
+// document.write("<h2>Smallest Number in an array is "+small+"</h2>")
+
+//TASK 10
+// var num=5;
+// var i;
+// for(i=1; (num*i)<=100; i++ )
+// document.write(num*i+" ")
+
+                                        //CHAPTER 21-25
+//TASK 1
+// var fname = prompt("Enter First Name")
+// var lname = prompt("Enter Last Name")
+// var fullname = fname+lname;
+// alert("WElCOME! "+fullname);
+
+//TASK 2
+// var str = prompt("Enter your Favourite Mobile Phone Brand")
+// document.write("My favourite phone is: "+str+"<br> Length Of string: "+str.length)
+
+//TASK 3
+// var name = "PAKISTAN";
+// document.write(`STRING: ${name}<br>Index of ' N ' =  ${name.indexOf("N")}`)
+
+//TASK 4
+// var name = "Hello World";
+// document.write("STRING: "+name+"<br>Index of ' l ' =  "+name.lastIndexOf("l"))
+
+//TASK 5
+// var name = "Pakistani";
+// document.write("STRING: "+name+"<br>Charactrer at Index 3 s=  "+name.charAt(3))
+
+//TASK 6
+// var fname = prompt("Enter First Name")
+// var lname = prompt("Enter Last Name")
+// var fullname = fname.concat( lname);
+// alert("WElCOME! "+fullname);
+
+//TASK 7
+// var name = "Hyderabad";
+// var rep = name.replace("Hyder","islam")
+// document.write("City: "+name+"<br>After replacement: "+rep)
+
+//TASK 8
+// var message = "Ali and Sami are best friends. They play cricket and football together.";
+// var rep = message.replace(/and/g,"&")
+// document.write("Message: "+message+"<br>After replacement: "+rep)
+
+//TASK 9
+// var str = "492";
+// var num = parseInt(str)
+// document.write("value: "+str+"<br>Type  : String <br>Value: "+num+"<br>Type : Number")
+
+//TASK 10
+// var str = prompt("Enter something");
+// var capital = str.toUpperCase()
+// document.write("<h2>User Input: "+str+"<br> UpperCase: "+capital+"</h2>")
+
+//TASK 11
+// var str = prompt("Enter something");
+// var first = str.charAt(0);
+// var capital = str.replace(first, first.toUpperCase())
+// document.write("<h2>User Input: "+str+"<br> UpperCase: "+capital+"</h2>");
+
+//TASK 12
+// var num = 35.36;
+// var str = num.toString();
+// var str = str.replace(".","")
+// document.write("<h2> Number: "+num+"<br> Result: "+str)
+
+//TASK 13
+// var userinp = prompt("Enter username:")
+// var i;
+// document.write("<h2>Username: "+userinp+"<br>")
+// for(i = 0; i<userinp.length; i++)
+// if(userinp.charAt(i) == "@" || userinp.charAt(i) == "." || userinp.charAt(i) == "!" || userinp.charAt(i) == ",")
+// alert("Please enter a valid user name ")
+
+//TASK 14
+// var arr = ["applepie", "cake", "cookie","chips","patties"];
+// var i ;
+// var inp = prompt("welcome to my bakery, what do you want to order sir/Ma'm ")
+// for(i=0; i<=inp.length; i++)
+// {
+// if(inp.charAt(i) === inp.charAt(i).toUpperCase())
+// inp = inp.toLowerCase();
+// }
+// for(i=0; i<=arr.length; i++)
+// {
+// if(inp === arr[i] ){
+// document.write("<h2>"+arr[i]+" is available at "+i+" in our bakery</h2>")
+// break;}
+// }
+// if(!(inp === arr[i]))
+// document.write("<h2>We are Sorry, "+inp+" is not available in our bakery</h2>")
+
+//TASK 15
+// var psd = prompt("Enter password")
+// var first = psd.charAt(0);
+// if(!(first.charCodAt(0) >= 48 && first.charCodeAt(0) <= 58) )
+//     if(psd.length == 6)
+//         for(i=0; i<psd.length;i++)
+//             for(j=0; j<)
+//            if((psd.charAt(i).charCodeAt(i) >= 65 && psd.charAt(i).charCodeAt(i) <= 90) || (psd.charAt(i+1).charCodeAt(i) >= 97 && psd.charAt(i).charCodeAt(i) <= 122 ))
+//                 if(psd.charAt(i+1).charCodeAt(i+1) >= 97 && psd.charAt(i+1).charCodeAt(i+1) <= 122 )
+// alert("Enter Valid Password");
+
+//TASK 16
+// var university = "University of Karachi";
+// var i;
+// for(i=0; i<university.length; i++)
+// document.write(university.charAt(i)+"<br>")
+
+//TASK 17
+// var userinp = prompt("User Input");
+// var lastIndex = userinp.length-1;
+// document.write("<h2>User Input: "+userinp+"<br>Last Character Of Input: "+userinp.charAt(lastIndex))
+
+//TASK 18
+// var str = "The quick brown fox jumps over the lazy  dog";
+// var count = 0;
+// var i, splitted="";
+// for (i=0; i<str.length; i++){
+// if(!(str.charAt(i)=== " ")){
+// var l = str.charAt(i)
+// splitted = splitted+l;}
+// else
+// splitted="";
+// if(splitted==="the" || splitted==="The")
+// count++;
+// }
+// document.write("<h2>TEXT: "+str+"<br>There are "+count+" occurence(s) of word \"the\" ")
+
+                                        //CHAPTER26-30
+//TASK 1
+// var uint = prompt("Enter a decimal value")
+// document.write("<h2>Number: "+uint+"<br>Round Off value: "+Math.floor(uint)+"<br>Floor value: "+Math.floor(uint)+"<br>ciel value: "+Math.ceil(uint))
+
+//TASK 2
+// var uint = prompt("Enter a decimal value")
+// document.write("<h2>Number: "+uint+"<br>Round Off value: "+Math.floor(uint)+"<br>Floor value: "+Math.floor(uint)+"<br>ciel value: "+Math.ceil(uint))
+
+//TASK 3
+// var uint = prompt("Enter a number: ")
+// var abso = Math.abs(uint);
+// document.write("<h2>The Absolute value of "+uint+" is "+abso)
+
+//TASK 4
+// var num = Math.random()
+// var intnum = (num*6)+1;
+// var dice = Math.floor(intnum)
+// document.write("<h2>The dice random value is "+dice)
+
+//TASK 5
+// var num = Math.random()
+// var intnum = (num*100)+1;
+// var dice = Math.floor(intnum)
+// document.write("<h2>The dice random number between 100 and 1 is: "+dice)
+
+//Task 7
+// var inp = prompt("Enter your weight in Kilograms")
+// var w = parseFloat(inp);
+// document.write("<h2>the weight of the user is "+w+" kilograms")
+
+//TASK 8
+// var rnd = (Math.random()*10)+1;
+// var guess  = Math.floor(rnd)
+// var inp = prompt("Guess a Number between 1 and 10")
+// if(inp === guess)
+// alert("Hurrah!!! Correct")
+// else if(inp == (guess+1) || inp ==(guess-1))
+// alert("close enough")
+// else if(!(inp === guess))
+// alert("Try Again! it was"+guess)
+
+                                        //CHAPTER 31-34
+//TASK 1
+// var d = new Date();
+// document.write("<h2>"+d)
+
+//TASK 2
+// var d = new Date();
+// var mnth = ["january","Febuarary","March","April","May","June","July","August","September","October","November","December"]
+// var m = d.getMonth();
+// document.write("<h2>Current Month: "+mnth[m])
+
+//TASK 3
+// var d = new Date();
+// var dn = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+// var day = d.getDay();
+// document.write("<h2>Today is "+dn[day])
+
+//TASK 4
+// var d = new Date();
+// var dn = ["Sun","Mon","Tue","Wed","Thu","Fri","Sat"]
+// var day = d.getDay();
+// if(day == 0 || day == 6)
+// document.write("<h2>It's Fun day")
+// else
+// document.write("<h2>Today is "+dn[day])
+
+//TASK 5
+// var d = new Date();
+// var dt = d.getDate();
+// if(dt <= 16)
+// document.write("<h2>Last Fifteen days of the Month")
+// else
+// document.write("<h2>last days of the month ")
+
+//TASK 6
+// var d = new Date();
+// var ms = d.getTime();
+// var mins = ms/6000000;
+// document.write("<h2>Current date: "+d)
+// document.write("<h2>Ellapsed Milliseconds since january 1, 1970: "+ms)
+// document.write("<h2>Ellapsed Minutes since january 1, 1970: "+mins)
+
+//TASK 7
+// var d = new Date();
+// var hrs  = d.getHours();
+// if(hrs >= 12)
+// document.write("<h2>It's AM")
+// else
+// document.write("<h2>It's PM")
+
+//TASK 8
+// var later = new Date("December 31, 2020" )
+// document.write("<h2>Later date:  "+later)
+
+//TASK 9
+// var rmz = new Date("april 25, 2020" )
+// var now = new Date()
+// var drmz = rmz.getTime()/(1000*60*60*24)
+// var dnow = now.getTime()/(1000*60*60*24)
+// var day_passed = Math.floor(dnow - drmz)
+// document.write("<h2>"+day_passed+" days have passed since 1st Ramadan 2020")
+
+//TASK 10
+// var dt = new Date("january 1, 2020" )
+// var now = new Date()
+// var ddt = dt.getTime()/(1000*60)
+// var dnow = now.getTime()/(1000*60)
+// var sec_passed = Math.floor(dnow - ddt)
+// document.write("<h2>ON REFERENCE DATE "+now+", <br>"+sec_passed+" seconds had passed since  beginning of 2020")
+
+//TASK 11
+// var d = new Date()
+// document.write("<h2>Current date: "+d)
+// var dhrs = d.getHours()
+// var hrs = d.setHours(dhrs-1)
+// document.write("<h2> 1 Hour ago, It was "+d)
+
+//TASK 12
+// var d = new Date()
+// document.write("<h2>Current date: "+d)
+// var dyrs = d.getFullYear()
+// var yrs = d.setFullYear(dyrs-100)
+// document.write("<h2> 100 years back, It was "+d)
+
+//TASK 13
+var inpage = +prompt("Enter Your age")
+var d = new Date()
+var dyrs = d.getFullYear()
+var yrs = dyrs - inpage
+document.write("<h2>Your age is "+inpage+"<br>Your birth year is "+yrs)
